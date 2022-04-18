b2d25c11d8b15d652aa104e2f93099de2189076a
ericrobert93 <ericrobert93@gmail.com>
2022-04-18 09:02:21 -0300

    Desafio 2 NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import './components/NavBar/NavBar.jsx';
+import NavBar from './components/NavBar/NavBar.jsx';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <NavBar/>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,8 +19,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
+
   );
 }
 
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,10 +1,40 @@
 import React from 'react';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './NavBar.css';
 function NavBar(props) {
     return (
-        <div>
-            
-        </div>
+    <>
+      <div className="containerNavBar titulares">
+        <h1 className="">Pet Shop Bruno</h1>
+        <ul className="nav justify-content-center">
+          <li className="nav-item">
+            <a className="nav-link bg-light itemNav" href="# top">
+              PRODUCTOS
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link bg-light itemNav" href="# top">
+              ¿DUDAS?
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link bg-light itemNav" href="# top">
+              GUÍA DE COMPRA
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link bg-light itemNav" href="# top">
+              CONTACTO
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link bg-light itemNav" href="# top">
+              GUÍA DE TALLES
+            </a>
+          </li>
+        </ul>
+      </div>
+      </> 
     );
 }
 
